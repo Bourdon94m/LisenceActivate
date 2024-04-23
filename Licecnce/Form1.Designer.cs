35cc85fe472e89b86849ada1f1ad7c3af577438a
Bourdon94m <105060610+Bourdon94m@users.noreply.github.com>
2024-04-23 22:36:05 +0200

    Ajoutez des fichiers projet.

---
+++ Licecnce/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Licecnce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxIdentifiant = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxActivate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(292, 239);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(109, 34);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxIdentifiant
+            // 
+            this.textBoxIdentifiant.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxIdentifiant.Location = new System.Drawing.Point(225, 108);
+            this.textBoxIdentifiant.Multiline = true;
+            this.textBoxIdentifiant.Name = "textBoxIdentifiant";
+            this.textBoxIdentifiant.Size = new System.Drawing.Size(258, 28);
+            this.textBoxIdentifiant.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Identifiant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Activate code";
+            // 
+            // textBoxActivate
+            // 
+            this.textBoxActivate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxActivate.Location = new System.Drawing.Point(225, 167);
+            this.textBoxActivate.Multiline = true;
+            this.textBoxActivate.Name = "textBoxActivate";
+            this.textBoxActivate.Size = new System.Drawing.Size(258, 28);
+            this.textBoxActivate.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 363);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxActivate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIdentifiant);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxIdentifiant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxActivate;
+    }
+}
+
---
+++ Licecnce/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Licecnce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            List<char> allCharacters = new List<char>();
+            List<char> randomCharacters = new List<char>();
+
+
+            for (char c = 'A'; c <= 'Z'; c++) { allCharacters.Add(c); }
+            for (char c = 'a'; c <= 'z';  c++) { allCharacters.Add(c); }
+            for (char c = '0'; c < '9'; c++) { allCharacters.Add(c); }
+
+            
+
+            for (int i=0; i < 14; i++)
+            {
+                // Générer un index aléatoire dans la plage des caractères disponibles
+                int randomIndexId = random.Next(0, allCharacters.Count);
+                // Ajouter le caractère correspondant à l'index aléatoire à la liste de caractères aléatoires
+                randomCharacters.Add(allCharacters[randomIndexId]);
+            }
+
+            string randomString = new string(randomCharacters.ToArray());
+
+
+            textBoxIdentifiant.Text = randomString;
+
+
+            for (int i = 0; i < 18; i++)
+            {
+                // Générer un index aléatoire dans la plage des caractères disponibles
+                int randomIndexLisence = random.Next(0, allCharacters.Count);
+                // Ajouter le caractère correspondant à l'index aléatoire à la liste de caractères aléatoires
+                randomCharacters.Add(allCharacters[randomIndexLisence]);
+            }
+
+            randomString = new string(randomCharacters.ToArray());
+
+            textBoxActivate.Text = randomString;
+
+        }
+    }
+}
